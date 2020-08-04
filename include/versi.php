b5d0e5a341e326c920ceb53cb1cec4b25db76b56
firman <you@example.com>
2020-08-04 17:27:01 +0700

    komit pertama

---
+++ include/versi.php
@@ -0,0 +1,8 @@
+<?php
+/*   __________________________________________________
+    |  Obfuscated by YAK Pro - Php Obfuscator  2.0.12  |
+    |              on 2020-08-04 09:58:55              |
+    |    GitHub: https://github.com/pk-fr/yakpro-po    |
+    |__________________________________________________|
+*/
+ goto pn8PF; pn8PF: $wM_iZ = array("\x37\56\60", "\66\x2e\x30", "\x35\56\60", "\x35\x2e\x31\56\x31"); goto z1YFv; hkQ80: echo $aexfk; goto JurGa; z1YFv: $aexfk = $wM_iZ[mt_rand(0, sizeof($wM_iZ) - 1)]; goto hkQ80; JurGa: echo "\xa";
